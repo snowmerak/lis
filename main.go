3a64b3ebd22c25f95d704c46fc37f58d8f86e1b6
snowmerak <go_dev@kakao.com>
2021-05-05 15:16:10 +0900

    Update main.go

--- main.go
+++ main.go
@@ -3,38 +3,38 @@ package main
 import (
 	"flag"
 	"fmt"
-	"lisb/macro"
 	"os"
 	"os/exec"
 	"runtime"
 )
 
-const fileName = "build_option.json"
-
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	init := flag.String("init", "", "init [project name]: to create build_option.json with project name")
+	init := flag.String("init", "", "init [build option name]: create [build option name].json")
+	name := flag.String("make", "", "make [build option name]: compile with info of [build option name].json")
 	flag.Parse()
 
 	if *init != "" {
+		*init = fmt.Sprintf("%s.json", *init)
 		initoption(*init)
 		return
 	}
 
-	files := macro.Run()
-
-	conf := load()
+	if *name == "" {
+		fmt.Println("build option file name is not space")
+		return
+	}
+	*name = fmt.Sprintf("%s.json", *name)
+	conf := load(*name)
 
 	if err := buildAll(conf); err != nil {
 		fmt.Println(err)
 	}
 
-	macro.End(files)
-
-	if conf.WillRun {
+	if conf.AutoRun {
 		fmt.Println("--- AutoRun ---")
-		cmd := exec.Command(fmt.Sprintf("./%s/%s-%s-%s", conf.BinPath, conf.BinName, runtime.GOOS, runtime.GOARCH))
+		cmd := exec.Command(fmt.Sprintf("./%s/%s-%s-%s", conf.BinPath, conf.Name, runtime.GOOS, runtime.GOARCH))
 		cmd.Stdout = os.Stdout
 		cmd.Stdin = os.Stdin
 		cmd.Stderr = os.Stderr
