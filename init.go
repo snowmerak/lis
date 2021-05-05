3e65e4aa7194d5a068596a517c284e055f70fbd2
snowmerak <go_dev@kakao.com>
2021-05-05 15:16:13 +0900

    Update init.go

--- init.go
+++ init.go
@@ -4,20 +4,69 @@ import (
 	"encoding/json"
 	"log"
 	"os"
+
+	"github.com/AlecAivazis/survey/v2"
 )
 
-func initoption(name string) {
+func initoption(fileName string) {
 	conf := &config{
 		BinPath: "bin",
-		BinName: name,
+		Name:    "temp",
 		Target: map[string][]string{
 			"windows": {"amd64", "386"},
 			"darwin":  {"amd64", "arm64"},
 			"linux":   {"amd64", "386", "arm64"},
 		},
 		GOGC:     150,
-		IsPlugin: false,
+		ToPlugin: false,
+		Module:   false,
 	}
+	qs := []*survey.Question{
+		{
+			Name: "Name",
+			Prompt: &survey.Input{
+				Message: "input project name: ",
+				Default: "demo",
+			},
+		},
+		{
+			Name: "BinPath",
+			Prompt: &survey.Input{
+				Message: "input path of compiled file: ",
+				Default: "bin",
+			},
+		},
+		{
+			Name: "GOGC",
+			Prompt: &survey.Select{
+				Message: "choose gogc: ",
+				Default: "150",
+				Options: []string{"off", "50", "100", "150", "200", "250"},
+			},
+		},
+		{
+			Name: "AutoRun",
+			Prompt: &survey.Confirm{
+				Message: "do you want to run app after compile? ",
+				Default: false,
+			},
+		},
+		{
+			Name: "ToPlugin",
+			Prompt: &survey.Confirm{
+				Message: "do you want to compile to plugin? ",
+				Default: false,
+			},
+		},
+		{
+			Name: "Module",
+			Prompt: &survey.Confirm{
+				Message: "will you use go111module? ",
+				Default: true,
+			},
+		},
+	}
+	survey.Ask(qs, conf)
 	if _, err := os.Stat(fileName); !os.IsNotExist(err) {
 		if err := os.Remove(fileName); err != nil {
 			log.Fatal(err)
