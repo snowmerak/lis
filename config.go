a24b5f4497307868d32fcc08bf957a7515725586
snowmerak <snowmerak@outlook.com>
2023-01-26 23:07:59 +0900

    distribute test bench build config

--- config.go
+++ config.go
@@ -1,6 +1,6 @@
 package main
 
-type config struct {
+type BuildConfig struct {
 	BinPath  string              `json:"bin_path" yaml:"bin_path"`
 	Name     string              `json:"name" yaml:"name"`
 	Target   map[string][]string `json:"target" yaml:"target"`
@@ -8,4 +8,20 @@ type config struct {
 	ToPlugin bool                `json:"to_plugin" yaml:"to_plugin"`
 	Module   bool                `json:"module" yaml:"module"`
 	Etc      []string            `json:"etc" yaml:"etc"`
+}
+
+type TestConfig struct {
+	Targets []struct {
+		Package string   `json:"package" yaml:"package"`
+		Test    []string `json:"test" yaml:"test"`
+	} `json:"targets" yaml:"targets"`
+	TestFlags []string `json:"flags" yaml:"flags"`
+}
+
+type BenchConfig struct {
+	Targets []struct {
+		Package string   `json:"package" yaml:"package"`
+		Bench   []string `json:"bench" yaml:"bench"`
+	} `json:"targets" yaml:"targets"`
+	BenchFlags []string `json:"flags" yaml:"flags"`
 }
